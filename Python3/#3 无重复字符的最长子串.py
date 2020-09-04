ece914f00086c2db7211ea83b68854c77b20be63
Mr_Q722 <58413657+MrQ722@users.noreply.github.com>
2020-09-04 15:42:11 +0800

    Update #3 无重复字符的最长子串.py

--- Python3/#3 无重复字符的最长子串.py
+++ Python3/#3 无重复字符的最长子串.py
@@ -18,3 +18,40 @@ class Solution:
                     l.append(s[i])
             num.append(len(l))
         return max(num)
+
+    # 考虑尽量在遍历一次的情况下尽量少建立list，并且尽量少索引
+    # 运行时间92ms，消耗内存13.8MB
+    class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        s = list(s)
+        num = 0
+        i, j = 0, 0
+        while j < len(s) :
+            if s[j] in s[i:j]:
+                if j-i > num:
+                    num = j-i
+                i = i + s[i:j].index(s[j]) + 1
+                j += 1
+            else:
+                j += 1
+        if j-i > num:
+            num = j-i
+        return num
+    
+# 考虑不使用list，直接在字符串上操作
+# 运行时间72ms，消耗内存13.5MB
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        num = 0
+        i, j = 0, 0
+        while j < len(s) :
+            if s[j] in s[i:j]:
+                if j-i > num:
+                    num = j-i
+                i = i + s[i:j].index(s[j]) + 1
+                j += 1
+            else:
+                j += 1
+        if j-i > num:
+            num = j-i
+        return num
