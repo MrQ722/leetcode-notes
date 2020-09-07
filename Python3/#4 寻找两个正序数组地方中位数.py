6642ae1dc34816d59e5b8764a9d6d49ff3286227
Mr_Q722 <58413657+MrQ722@users.noreply.github.com>
2020-09-07 21:17:06 +0800

    Update #4 寻找两个正序数组地方中位数.py

--- Python3/#4 寻找两个正序数组地方中位数.py
+++ Python3/#4 寻找两个正序数组地方中位数.py
@@ -1,7 +1,7 @@
 # 最初的想法是分别从两个列表的两端进行比较，比较次数肯定小于两个list和的一半
 # 但是由于两个list的和可能为奇数或者偶数，因此需要很多的分支语句，放弃了这种想法
 # 最后，应用list自带的sort()语句对两个list的拼接进行排序，直观求出中位数
-# 
+# 运行时间56ms，消耗内存13.7MB
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         nums = nums1 + nums2
