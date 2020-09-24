2410347dd67bc86ac633ae4c546e82d915646f78
Mr_Q722 <58413657+MrQ722@users.noreply.github.com>
2020-09-24 09:33:55 +0800

    Create #5 最长回文子串.py

---
+++ Python3/#5 最长回文子串.py
@@ -0,0 +1,30 @@
+# 没想到什么好方法，就直接遍历了
+# 执行用时： 1152ms，击败73.68%
+# 内存消耗： 13.6MB，击败48.74%
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if not s :
+            return s
+        num = []
+        for i in range(len(s)):
+            if i+2 <len(s) and s[i] == s[i+2]:
+                num.append(self.helper(i,i+2,s))
+            if i+1 <len(s) and s[i] == s[i+1]:
+                num.append(self.helper(i,i+1,s))
+        if not num:
+            return s[0]
+        start = max(num)[1] 
+        L = max(num)[0]   
+        print(num)
+        return   s[start:start+L]   
+    def helper(self,i,j,s):
+        a = i-1
+        b = j+1
+        while a >= 0 and b < len(s):
+            if s[a] == s[b]:
+                a -= 1
+                b += 1
+            else:
+                break
+        return (b-a-1,a+1)
