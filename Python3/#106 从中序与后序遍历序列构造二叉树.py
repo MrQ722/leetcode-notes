b4d3a6655dbef33e212a452af43708d8275d55ed
Mr_Q722 <58413657+MrQ722@users.noreply.github.com>
2020-09-25 16:14:31 +0800

    Update #106 从中序与后序遍历序列构造二叉树.py

--- Python3/#106 从中序与后序遍历序列构造二叉树.py
+++ Python3/#106 从中序与后序遍历序列构造二叉树.py
@@ -30,4 +30,35 @@ class Solution:
             tree.right = self.buildTree(newin,newpost)
         return tree
         
-#
+# 简化后
+# 执行用时：208ms，击败33.60%
+# 内存消耗：85.5MB，击败47.39%
+
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+        if not inorder :
+            return None
+        tree = TreeNode(postorder[-1])
+        index = inorder.index(tree.val)
+        tree.left = self.buildTree(inorder[:index],postorder[:index])
+        tree.right = self.buildTree(inorder[index+1:],postorder[index:-1])
+        return tree
+
+# 简单起见，递归的时候每次我都开辟了新的数组，这个其实是没有必要的
+# 我们可以通过四个变量来记录inorder和postorder的起始位置即可
+# 执行用时：100ms，击败76.24%
+# 内存消耗：17.4MB，击败93.68%
+
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+        def dfs(inorder, in_start, in_end, postorder, post_start, post_end):
+            if in_start > in_end: return None
+            if in_start == in_end: return TreeNode(inorder[in_start])
+            if post_start == post_end: return TreeNode(inorder[in_start])
+            root = TreeNode(postorder[post_end])
+            i = inorder.index(root.val)
+            root.left = dfs(inorder, in_start, i - 1, postorder, post_start, post_start + i - 1 - in_start)
+            root.right = dfs(inorder, i + 1, in_end, postorder, post_start + i - 1 - in_start + 1, post_end - 1)
+            return root
+        n = len(inorder)
+        return dfs(inorder, 0, n - 1, postorder, 0, n - 1)
