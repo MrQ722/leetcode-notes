434398127b4a3f8eeffedf81ef3b9b0f2c490e6d
Mr_Q722 <58413657+MrQ722@users.noreply.github.com>
2020-09-29 15:17:27 +0800

    Update #20 有效的括号.py

--- Python3/#20 有效的括号.py
+++ Python3/#20 有效的括号.py
@@ -20,3 +20,13 @@ class Solution:
             return False
         else:
             return True
+
+# 简化代码
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dic = {'{': '}',  '[': ']', '(': ')', '?': '?'}
+        stack = ['?']
+        for c in s:
+            if c in dic: stack.append(c)
+            elif dic[stack.pop()] != c: return False 
+        return len(stack) == 1
