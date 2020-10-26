8a6507cf16e64ae422044bf1cad3184ecf704398
Mr_Q722 <58413657+MrQ722@users.noreply.github.com>
2020-10-26 09:58:12 +0800

    Create #234 回文链表.py

---
+++ Python3/#234 回文链表.py
@@ -0,0 +1,98 @@
+# 双指针
+# 执行用时：92ms，击败36.40%
+# 内存消耗：23.2MB，击败61.05%
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        if not head: return True
+        val = []
+        while head:
+            val.append(head.val)
+            head = head.next
+        i,j=0,-1
+        for n in range(len(val)//2):
+            if val[i] == val[j]:
+                i+=1
+                j-=1
+            else:
+                return False
+        return True
+
+
+# 简化写法，指针可以用list反向
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        vals = []
+        current_node = head
+        while current_node is not None:
+            vals.append(current_node.val)
+            current_node = current_node.next
+        return vals == vals[::-1]
+
+
+# 递归
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+
+        self.front_pointer = head
+
+        def recursively_check(current_node=head):
+            if current_node is not None:
+                if not recursively_check(current_node.next):
+                    return False
+                if self.front_pointer.val != current_node.val:
+                    return False
+                self.front_pointer = self.front_pointer.next
+            return True
+
+        return recursively_check()
+
+# 快慢指针
+class Solution:
+
+    def isPalindrome(self, head: ListNode) -> bool:
+        if head is None:
+            return True
+
+        # 找到前半部分链表的尾节点并反转后半部分链表
+        first_half_end = self.end_of_first_half(head)
+        second_half_start = self.reverse_list(first_half_end.next)
+
+        # 判断是否回文
+        result = True
+        first_position = head
+        second_position = second_half_start
+        while result and second_position is not None:
+            if first_position.val != second_position.val:
+                result = False
+            first_position = first_position.next
+            second_position = second_position.next
+
+        # 还原链表并返回结果
+        first_half_end.next = self.reverse_list(second_half_start)
+        return result    
+
+    def end_of_first_half(self, head):
+        fast = head
+        slow = head
+        while fast.next is not None and fast.next.next is not None:
+            fast = fast.next.next
+            slow = slow.next
+        return slow
+
+    def reverse_list(self, head):
+        previous = None
+        current = head
+        while current is not None:
+            next_node = current.next
+            current.next = previous
+            previous = current
+            current = next_node
+        return previous
+
