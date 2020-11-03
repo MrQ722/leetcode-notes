c800b642e54d76316cf75d9ca468f071f41b67bb
Mr_Q722 <58413657+MrQ722@users.noreply.github.com>
2020-11-03 17:16:45 +0800

    Create #349 两个数组的交集.py

---
+++ Python3/#349 两个数组的交集.py
@@ -0,0 +1,51 @@
+# 随机遍历一个表
+# 执行用时：68ms，击败42.91%
+# 内存消耗：13.6MB，击败9.42%
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1 = list(set(nums1))
+        nums2 = list(set(nums2))
+        if min(len(nums1),len(nums2)) == 0 :
+            return []
+        sub = []
+        for i in nums1:
+            if i in nums2:
+                sub.append(i)
+        return sub
+# 简化
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        set1 = set(nums1)
+        set2 = set(nums2)
+        return self.set_intersection(set1, set2)
+
+    def set_intersection(self, set1, set2):
+        if len(set1) > len(set2):
+            return self.set_intersection(set2, set1)
+        return [x for x in set1 if x in set2]
+
+
+
+# 排序+双指针
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1.sort()
+        nums2.sort()
+        length1, length2 = len(nums1), len(nums2)
+        intersection = list()
+        index1 = index2 = 0
+        while index1 < length1 and index2 < length2:
+            num1 = nums1[index1]
+            num2 = nums2[index2]
+            if num1 == num2:
+                # 保证加入元素的唯一性
+                if not intersection or num1 != intersection[-1]:
+                    intersection.append(num1)
+                index1 += 1
+                index2 += 1
+            elif num1 < num2:
+                index1 += 1
+            else:
+                index2 += 1
+        return intersection
