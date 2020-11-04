7a33d28c070c56b0139d2cdd3ae1bfb9bad66501
Mr_Q722 <58413657+MrQ722@users.noreply.github.com>
2020-11-04 22:52:52 +0800

    Update #140 单词拆分II.py

--- Python3/#140 单词拆分II.py
+++ Python3/#140 单词拆分II.py
@@ -41,3 +41,29 @@ class Solution:
         breakList = backtrack(0)
         return [" ".join(words[::-1]) for words in breakList]
 
+
+    
+# 回溯+记忆字符串是否可分
+class Solution:
+    # 带备忘录的记忆化搜索
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        res = []
+        memo = [1] * (len(s)+1)
+        wordDict = set(wordDict)
+
+        def dfs(wordDict,temp,pos):
+            num = len(res)                  # 回溯前先记下答案中有多少个元素
+            if pos == len(s):
+                res.append(" ".join(temp))
+                return
+            for i in range(pos,len(s)+1):
+                if memo[i] and s[pos:i] in wordDict: # 添加备忘录的判断条件
+                    temp.append(s[pos:i])
+                    dfs(wordDict,temp,i)
+                    temp.pop()
+            # 答案中的元素没有增加，说明s[pos:]不能分割，修改备忘录        
+            memo[pos] = 1 if len(res) > num else 0 
+            
+        dfs(wordDict,[],0)
+        return res
+
