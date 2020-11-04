c646f6d0235dd936695e42311203f8444a0ef937
Mr_Q722 <58413657+MrQ722@users.noreply.github.com>
2020-11-04 16:28:16 +0800

    Update #57 插入区间.py

--- Python3/#57 插入区间.py
+++ Python3/#57 插入区间.py
@@ -35,3 +35,28 @@ class Solution:
         sub.append([start,end])
         sub.sort()
         return sub
+
+
+# 简化版本
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        i = 0 
+        n = len(intervals)
+        res = []
+        # 找左边重合区域
+        while i < n and newInterval[0] > intervals[i][1]:
+            res.append(intervals[i])
+            i += 1
+        tmp = [newInterval[0], newInterval[1]]
+        # 找右边重合区域
+        while i < n and newInterval[1] >= intervals[i][0]:
+            tmp[0] = min(tmp[0], intervals[i][0])
+            tmp[1] = max(tmp[1], intervals[i][1])
+            i += 1
+        res.append(tmp)
+        while i < n :
+            res.append(intervals[i])
+            i += 1
+        return res
+
+    
