e72866e55c347a592c2bc7713874f616ab6627b6
Mr_Q722 <58413657+MrQ722@users.noreply.github.com>
2020-12-01 21:09:30 +0800

    Update #34 在排序数组中查找元素的第一个和最后一个位置.py

--- Python3/#34 在排序数组中查找元素的第一个和最后一个位置.py
+++ Python3/#34 在排序数组中查找元素的第一个和最后一个位置.py
@@ -22,10 +22,12 @@ class Solution(object):
             n = len(nums)-1
             left = 0
             right = n
-            while(left<=right):
+            while(left<right):
                 mid = (left+right)//2
-                if nums[mid] >= target:
+                if nums[mid] > target:
                     right = mid-1
+                if nums[mid] == target:
+                    right = mid
                 if nums[mid] < target:
                     left = mid+1
             return left
