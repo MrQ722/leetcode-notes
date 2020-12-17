443a31f5535ad736ee19529725979204ad6cb7c9
Mr_Q722 <58413657+MrQ722@users.noreply.github.com>
2020-12-17 16:34:38 +0800

    Update 07.重建二叉树.py

--- 剑指offer/07.重建二叉树.py
+++ 剑指offer/07.重建二叉树.py
@@ -1,1 +1,20 @@
-# 
+# 递归
+# 执行用时：192ms，击败45.17%
+# 内存消耗：86.9MB，击败5.07%
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        if not preorder:return None
+        tree = TreeNode(preorder[0])
+        node = tree
+        head = inorder.index(preorder[0])
+        node.left = self.buildTree(preorder[1:head+1],inorder[:head])
+        node.right = self.buildTree(preorder[1+head:],inorder[1+head:])
+        return tree
